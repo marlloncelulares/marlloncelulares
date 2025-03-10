f23a4ca2402e9e72476cc757b7d4e211640961fb
marlloncelulares <marlloncelulares@gmail.com>
2025-03-10 19:09:06 -0300

    Update send

--- app/api/sendSchedule/route.ts
+++ app/api/sendSchedule/route.ts
@@ -2,19 +2,17 @@ import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 export async function POST(request: Request) {
-  // Configurar o transporte do Nodemailer
   const transporter = nodemailer.createTransport({
-    host: 'smtp.titan.email', // Servidor SMTP do Titan Mail
-    port: 587, // Porta padrão para envio SMTP com TLS
-    secure: false, // TLS
+    host: 'smtp.titan.email',
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER, // Endereço de e-mail Titan
-      pass: process.env.EMAIL_PASS, // Senha do Titan Mail
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   try {
-    // Receber os dados do cliente
     const body = await request.json();
     const { to, subject, text, html } = body;
 
@@ -22,7 +20,6 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: 'Campos obrigatórios ausentes' }, { status: 400 });
     }
 
-    // Enviar e-mail
     await transporter.sendMail({
       from: `"Claudinho Celulares" <${process.env.EMAIL_USER}>`,
       to,
